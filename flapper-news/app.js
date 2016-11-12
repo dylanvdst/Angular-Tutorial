39b479e174f46d7ebda1bf27f6ca1c073e8cb5b2
dylanvdst <dylan.vandesteene.u8267@student.hogent.be>
2016-11-12 19:25:38 +0100

    Creating Schemas With Mongoose

--- flapper-news/app.js
+++ flapper-news/app.js
@@ -44,3 +44,8 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+var mongoose = require('mongoose');
+require('./models/Posts');
+require('./models/Comments');
+mongoose.connect('mongodb://localhost/news');
