68494582eafd155b398f9d89be4021e3b8f75095
dylanvdst <dylan.vandesteene.u8267@student.hogent.be>
2016-11-12 20:39:01 +0100

    Loading Posts

--- flapper-news/public/javascripts/angularApp.js
+++ flapper-news/public/javascripts/angularApp.js
@@ -8,7 +8,12 @@ app.config([
     $stateProvider.state('home',{
       url: '/home',
       templateUrl: '/home.html',
-      controller: 'MainCtrl'
+      controller: 'MainCtrl',
+      resolve: {
+        postPromise: ['posts', function(posts){
+          return posts.getAll();
+        }]
+      }
     });
 
     $stateProvider.state('posts',{
@@ -20,16 +25,15 @@ app.config([
     $urlRouterProvider.otherwise('home');
   }]);
 
-app.factory('posts', [function(){
+app.factory('posts', ['$http', function($http){
   var o = {
-    posts: [
-      {title: 'post 1', upvotes: 5},
-      {title: 'post 2', upvotes: 2},
-      {title: 'post 3', upvotes: 15},
-      {title: 'post 4', upvotes: 9},
-      {title: 'post 5', upvotes: 4}
-    ]
+    posts: []
   };
+  o.getAll = function(){
+    return $http.get('/posts').success(function(data){
+      angular.copy(data, o.posts);
+    })
+  }
   return o;
 }]);
 
