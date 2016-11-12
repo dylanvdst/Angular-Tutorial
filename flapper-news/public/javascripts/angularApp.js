4f0ea412600fa5e3ca025e3e140b0baaf69a98c9
dylanvdst <dylan.vandesteene.u8267@student.hogent.be>
2016-11-12 20:47:44 +0100

    Upvoting Posts

--- flapper-news/public/javascripts/angularApp.js
+++ flapper-news/public/javascripts/angularApp.js
@@ -42,6 +42,12 @@ app.factory('posts', ['$http', function($http){
     });
   };
 
+  o.upvote = function(post) {
+  return $http.put('/posts/' + post._id + '/upvote').success(function(data){
+      post.upvotes += 1;
+    });
+};
+
   return o;
 }]);
 
@@ -67,7 +73,7 @@ function($scope, posts){
 
   $scope.incrementUpvotes = function(post)
   {
-    post.upvotes += 1;
+    posts.upvote(post);
   };
 }]);
 
